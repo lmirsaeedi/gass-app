47594066fd857be98a1e07e63435d7e3a216ad97
lmirsaeedi <lmirsaeedi@gmail.com>
2025-04-09 16:04:44 +0330

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,6 @@ st.title('🎈 App Name')
 
 st.write('Hello world!')
 
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
 from sklearn import datasets, ensemble
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import mean_squared_error
